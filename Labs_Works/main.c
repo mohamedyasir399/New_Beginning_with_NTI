bf2957411cc3c022e1dad50091122ac1e9aad6bd
mohamedyasir399 <mohmedyasser405@gmail.com>
2023-09-23 11:53:10 +0300

    modify some line in App.c

--- Labs_Works/main.c
+++ Labs_Works/main.c
@@ -1,29 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "queue_struct.h"
-int main()
-{
-    char inputWprd[50],res[50];
-    int count =0;
-    gets(inputWprd);
-    queue_t *Qu = init_queue(strlen(inputWprd));
-    for(int i =0;i<strlen(inputWprd);i++)
-    {
-        //printf("iam %d  %c\n",i,inputWprd[i]);
-        if(inputWprd[i]=='*')
-        {
-            deQueue(Qu,&res[count]);
-           // printf("iam pop - %c \n",res[count]);
-            count++;
-        }else if(inputWprd[i]==' ')
-        {
-            continue;
-        }else{
-            enQueue(Qu,inputWprd[i]);
-        }
-    }
-    printf("the EnQueue word %s\n",res);
-    free_queue(Qu);
-   return 0;
+#define SI(p,n,m) float si; si=p*n*m/100;
+
+
+int main() {
+  int a[5]={5,1,15,20,25};
+
+  char i[30];
+  scanf("%s",i);
+  printf("%s",i);
+  return 0;
 }
